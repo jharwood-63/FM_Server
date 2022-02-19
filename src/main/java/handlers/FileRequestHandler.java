8e58ac6a7c0e939221b9739a14ef8eb9a0e0aeda
jharwood <jackson.harwood21@gmail.com>
2022-02-18 19:01:01 -0700

    Small change

--- src/main/java/handlers/FileRequestHandler.java
+++ src/main/java/handlers/FileRequestHandler.java
@@ -1,9 +1,14 @@
 package handlers;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
 
 public class FileRequestHandler implements HttpHandler {
     @Override
