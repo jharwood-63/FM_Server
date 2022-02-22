cdf5a69a7862da483c905648c1f94e446d7902a8
jharwood <jackson.harwood21@gmail.com>
2022-02-22 12:10:32 -0700

    Fix FileRequestHandler

--- src/main/java/handlers/FileRequestHandler.java
+++ src/main/java/handlers/FileRequestHandler.java
@@ -1,38 +1,37 @@
 package handlers;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.nio.file.Files;
-import java.util.Locale;
 
 
 public class FileRequestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
+        System.out.println("FileRequestHandler");
         try {
             if (exchange.getRequestMethod().toUpperCase().equals("GET")) {
                 String urlPath = exchange.getRequestURI().toString();
-                if (urlPath.equals("/") || urlPath == null) {
+                if (urlPath.equals("/")) {
                     urlPath = "/index.html";
                 }
 
                 String filePath = "web" + urlPath;
                 File file = new File(filePath);
-                if (!file.exists()) {
+                if (file.exists()) {
+                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
+                }
+                else {
                     exchange.sendResponseHeaders(HttpURLConnection.HTTP_NOT_FOUND, 0);
                     filePath = "web/HTML/404.html";
                     file = new File(filePath);
                 }
-                else {
-                    exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, 0);
-                }
 
-                OutputStream resBody = exchange.getResponseBody();
-                Files.copy(file.toPath(), resBody);
+                OutputStream respBody = exchange.getResponseBody();
+                Files.copy(file.toPath(), respBody);
+                respBody.close();
             }
         }
         catch (IOException e) {
